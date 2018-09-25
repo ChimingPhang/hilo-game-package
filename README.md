3ff2c3bb12de1c29f125b59922755da79244d1b5
chimingphang <chimingphang@gmail.com>
2018-09-25 16:37:23 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,10 @@
+### Package version of Hilo game framework
+
+Fork from [Hilo](https://github.com/hiloteam/Hilo)
+
+#### Usage 
+```
+npm i hilo-game-package
+
+import {Class, Hilo, Ease} from 'hilo-game-package'
+```
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-/**
- * The file enables `@/store/index.js` to import all vuex modules
- * in a one-shot manner. There should not be any reason to edit this file.
- */
+var files = require.context('.', true, /\.js$/);
+var modules = {};
 
-const files = require.context('.', false, /\.js$/)
-const modules = {}
-
-files.keys().forEach(key => {
-  if (key === './index.js') return
-  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
-
-export default modules
+files.keys().forEach(function (key) {
+  if (key === './index.js') return;
+  modules[key.split('/')[2].replace(/(\.\/|\.js)/g, '')] = files(key);
+});
+module.exports = modules;
